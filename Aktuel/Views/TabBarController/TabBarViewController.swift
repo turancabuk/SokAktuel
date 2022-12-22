c076fd7a8ebb4688ac9e3aa7b5d6d1507661a04f
turan <turan6677@gmail.com>
2022-12-22 19:29:48 +0300

    TabBar created.

---
+++ Aktuel/Views/TabBarController/TabBarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TabBarViewController.swift
+//  Aktuel
+//
+//  Created by Turan Çabuk on 22.12.2022.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
